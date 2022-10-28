72387be908462b76a9afc7a390071ea0aef57d34
Alwoch <salwoch@gmail.com>
2022-10-28 16:52:03 +0300

    feature(Home): add search input for tools

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,50 @@
-import Navbar from "../Nav/Navbar"
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Card from 'react-bootstrap/Card'
+import "./Home.css";
+import Navbar from "../Nav/Navbar";
 const Home = () => {
- return ( 
-  <>
-  <Navbar/>
-  </>
+  return (
+    <>
+      <Navbar />
+      <section className=".container-xxl">
+        <InputGroup className="mb-3">
+          <Form.Control
+            placeholder="search tool"
+            aria-label="search tool"
+            aria-describedby="basic-addon2"
+          />
+          <Button
+            className="button"
+            variant="outline-secondary"
+            id="button-addon2"
+          >
+            search
+          </Button>
+        </InputGroup>
+        <p className="home-info">
+          The Wikimedia tool below is missing some important information. can
+          you find it?
+        </p>
+        <p>
+          Click <span>I got this</span> to add this information or{" "}
+          <span>Next</span> to see another tool
+        </p>
+    <Card>
+      <Card.Header>Tool:</Card.Header>
+      <Card.Body>
+        <Card.Title>Pywikibot</Card.Title>
+        <Card.Text>
+          api_url
+        </Card.Text>
+        <Button variant="primary">I Got this!</Button>
+        <Button variant="primary">Next Tool</Button>
+      </Card.Body>
+    </Card>
+      </section>
+    </>
   );
-}
- 
+};
+
 export default Home;
--- src/components/Leaderboard/Profile.js
+++ src/components/Leaderboard/Profile.js
@@ -1,17 +1,40 @@
-const Profile = ({id,img,fullName,score}) => {
+import { useState, useEffect } from "react";
+import Table from "react-bootstrap/Table";
+import leaderboardData from "./leaderboardData";
+
+const Profile = () => {
+  const [profileData, setProfileData] = useState([]);
+
+  const loadProfiles = async () => {
+    await setProfileData(leaderboardData);
+  };
+
+  useEffect(() => {
+    loadProfiles();
+  }, []);
+
   return (
     <>
-      <div className="flex">
-        <div className="item">
-          <img src={img} alt="contributor profile" />
-            <div className="info">
-              <h3 className="name text-dark">{fullName}</h3>
-            </div>
-        </div>
-        <div className="item">
-          <span>{score}</span>
-        </div>
-      </div>;
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>username</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {profileData.map((each) => (
+            <tr key={each.id}>
+              <td>{each.id}</td>
+              <td>{each.fullName}</td>
+              <td>{each.username}</td>
+              <td>{each.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </>
   );
 };
